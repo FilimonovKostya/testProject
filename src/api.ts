41e48a0743ad5545e8926efd71f8615787532fcd
Filimonov Kostya <konstantinfilimonou@gmail.com>
2021-02-23 12:02:46 +0300

    add meta in index.html

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // baseURL: 'https://cors-anywhere.herokuapp.com/https://www.mrsoft.by/data.json',
-    baseURL: 'https://www.mrsoft.by/data.json',
+    baseURL: 'https://cors-anywhere.herokuapp.com/https://www.mrsoft.by/data.json',
 })
 
 //Использовал https://cors-anywhere.herokuapp.com/ чтобы обойти политику безопасности CORS, т.к я не могу слать запрос с другого домена
